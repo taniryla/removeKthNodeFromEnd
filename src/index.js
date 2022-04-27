c467ca8cd4ade684e74c6142a1d61c8c939a3af6
Ryland Taniguchi <ryland0000@gmail.com>
2022-04-27 20:29:30 +0100

    flow control logic pseudocode

--- src/index.js
+++ src/index.js
@@ -31,14 +31,28 @@ head, k
 
 // Step Two (Plan). Come up with concrete examples to help you understand the problem better. Find a connection between the data inputs and the unknown. You may be obliged to consider auxiliary problems if an immediate connection cannot be found.
 // 8.  Have you seen it before? Or have you seen the same problem in a slightly different form? Do you know a related problem?
+yes linked list construction remove
+
+linked list length
+
 // 9.   Do you know a coding strategy that could be useful? Look at the unknown! And try to think of a familiar problem having the same or a similar unknown. Here is a problem related to yours and solve before. Could you use it? Could you use its result? Could you use its method?
+
+traverse and remove node
+
 // 10. Start with a simple example. Could you restate the problem? Could you restate it still differently?
+
+see notes 
 // 11. What about examples with empty inputs? Any other edge case examples? What examples with invalid inputs?
+
+if kth is the head 
+
 // 12. Progress to more complex examples. What is your updated plan here to find a solution?
 // 13. If you still cannot solve the proposed problem, try to solve first some related problem. Could you imagine a more accessible related problem? A more general problem? A more special problem? An analogous problem? Could you solve a part of the problem? Keep only a part of the data inputs, drop the other part; how far is the unknown then determined, how can it vary? Could you derive something useful from the data? Could you think of other data appropriate to determine the unknown? Could you change the unknown or the data, or both if necessary, so that the new unknown and the new data are nearer to each other? Did you use all the data? Did you use the whole of the data inputs? Have you taken into account all essential notions involved in the problem?
 
 // Step Three (Solve). Carry out your plan of the solution and check each one of your steps in pseudocode.
 // 14.  Can you see clearly that the step is correct?
+
+see pseudocode
 
 // IV. 	Step Four (Check). Examine the solution obtained and refactor.
 // 15.  Can you check your result?
@@ -52,3 +66,14 @@ head, k
 // if nodeToBeRemoved = head, change it's value and next point 
 // don't change it as the head of the linked list
 
+// current equals head and count equals 0
+
+// traverse current til null and add counter
+
+// let position = count minus K plus 1
+
+// remove node (position)
+// while current.value !== position, remove
+// current.prev.next = current.next and current equals nulls
+
+// no return needed
